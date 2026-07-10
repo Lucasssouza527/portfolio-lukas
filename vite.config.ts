1ffe4546720242444058fed9b5fab60cb5b7fa3e
Lucas souza <202408484488@alunos.estacio.br>
2026-07-10 11:15:09 -0300

    build para produção

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Se o seu link é github.com/wolflks/portfolio-lukas
-  // Entao a base deve ser '/portfolio-lukas/'
+
   base: '/portfolio-lukas/', 
 })
